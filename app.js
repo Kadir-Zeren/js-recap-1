73bd2b80c66badbd6528482599f4c46fed11fb03
kadir-zeren <k4dzeren@gmail.com>
2023-11-19 21:20:24 +0300

    v2

---
+++ app.js
@@ -0,0 +1,45 @@
+// console.log("Hello World");
+
+// alert("bu bir alert dir");
+
+// result = confirm("are you sure");
+
+// console.log(result);
+
+// console.warn("warn");
+
+// console.error("danger");
+
+const pi = 3.14;
+
+console.log(pi);
+
+console.log(typeof pi);
+
+const number1 = "3";
+console.log(typeof sayi1);
+
+const isOpen = false;
+console.log(isOpen, typeof isOpen);
+
+let language = "java";
+console.log(language, typeof language);
+
+language = "Javascript";
+console.log(language);
+
+language = true;
+console.log(language);
+
+language = 3.14;
+
+let number;
+
+number = 23;
+console.log(typeof number);
+
+{
+  let local = 5;
+  local = local + 1;
+  console.log(local);
+}
